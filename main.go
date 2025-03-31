7995a45e786bf57ef5c82e001d9d3c90a4f79fef
Justin Lim <jjustinlim@Justins-MacBook-Air.local>
2025-03-31 14:44:42 -0700

    Initial structure added

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+}
+
+func main() {
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":80"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", HelloHandler)
+
+	log.Printf("server is listening at %s...", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
